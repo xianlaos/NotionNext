5bbe5c13be35d9d92bdcd8beb62281b2c303cf19
xianlaos <uumm678123@gmail.com>
2025-07-24 15:42:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '数字跳动博客', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '社区信息播报', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xx.4569999.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://4569999.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数字跳动博客, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
